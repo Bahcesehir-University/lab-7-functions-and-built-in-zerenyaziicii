59d0a9ead61d40181be04edcb8ce0c0206f76e21
zerenyaziicii <zerenyaziicii@gmail.com>
2026-04-17 15:44:01 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -24,6 +24,32 @@ using namespace std;
 //  Goal: quickly recall variables, cin, cout, and arithmetic.
 // ============================================================
 
+double celsiusToFahrenheit(double c){
+    return (c * 9.0 / 5.0) + 32;
+}
+
+bool isPrime(int n){
+    if(n <= 1) return false;
+    
+    for(int i = 2; i <= sqrt((double)n); i++){
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+int maxOfThree(int a, int b, int c) {
+   int maxValue = a; // diyorum kji en büyük değer a sonra kontrol ediyorum.
+   if(b > maxValue) maxValue = b;
+   if(c > maxValue) maxValue = c;
+   return maxValue;
+    }
+double average(double arr[], int size){
+    double sum = 0.0;
+    for(int i = 0; i < size ; i++){
+        sum += arr[i];
+    }
+    return sum / size;
+}
+
 int main()
 {
     // ----------------------------------------------------------
@@ -40,8 +66,11 @@ int main()
 
     // TODO: Ask the user to enter the radius
     // TODO: Read the radius into the variable 'radius'
+    // double area = PI * radius*radius; yapıp da yazdırabilirsin
     // TODO: Compute the area and print it
-
+    cout << "Enter a radius:";
+    cin >> radius;
+    cout << "area:" << radius*radius*PI;
 
 
 
@@ -53,7 +82,8 @@ int main()
     //  You define it ONCE, then CALL it as many times as you need.
     //
     //  Syntax:
-    //      returnType  functionName(paramType param, ...)
+    //      returnType  functionName(paramType param, ...) 
+    // bu fonksiyonun nasıl kurulduğu kısım
     //      {
     //          // body
     //          return value;   // omit if returnType is void
@@ -94,12 +124,17 @@ int main()
     double val = 17.5;
 
     cout << "\n=== Built-in Functions ===" << endl;
+    
+// bu built in functions zaten #include <cmath> bu kütüphane içinde tanımlı 
 
     // TODO: Print sqrt of val
+   cout << "sqrt(17.5) = " << sqrt(val) << endl;
     // TODO: Print ceil of val
+    cout << "ceil(17.5) = " << ceil(val) << endl;
     // TODO: Print floor of val
+    cout << "floor(17.5) = " << floor(val) << endl;
     // TODO: Print round of val
-
+    cout << "round(17.5) = " << round(val) << endl;
 
 
 
@@ -125,10 +160,11 @@ int main()
 
     // TODO: Ask the user for a Celsius value and read it
     // TODO: Call celsiusToFahrenheit and print the result
-
-
-
-
+    cout << " Enter temperature Celsius value: ";
+    cin >> celsius;
+    cout << celsius << "C = " << celsiusToFahrenheit(celsius) << "F " << endl;
+    // fonksiyonu yazarken mainin üstüne tanımla
+    
     // ----------------------------------------------------------
     // Exercise 3-B : isPrime
     //
@@ -145,9 +181,14 @@ int main()
     cout << "\n=== Prime Checker ===" << endl;
 
     // TODO: Ask the user for an integer and read it
+    cout << "Enter an integer : ";
+    cin >> number;
+    if (isPrime(number)){
+        cout << number << " is prime." << endl;
+    } else {
+        cout << number << "İs not prime." << endl;
+    }
     // TODO: Call isPrime and print "X is prime" or "X is not prime"
-
-
 
 
     // ----------------------------------------------------------
@@ -165,8 +206,9 @@ int main()
 
     // TODO: Ask the user for three integers and read them
     // TODO: Call maxOfThree and print the result
-
-
+cout << "Enter three integers: ";
+cin >> x >> y >> z;
+cout << "Maximum = " << maxOfThree(x,y,z) << endl;
 
 
     // ============================================================
@@ -199,7 +241,15 @@ int main()
     // TODO: Read 5 values from the user into the array
     // TODO: Call average() and store the result
     // TODO: Print the average, sqrt of average, and rounded average
-
+ cout << "Enter " << SIZE << " numbers : " << endl;
+ for(int i = 0; i <SIZE ; i++){
+     cout << "[" << i + 1 << "]";
+     cin >> grades [i];
+ }
+ double avg = average(grades , SIZE);
+ cout << "Average              =" << avg <<endl;
+ cout << "sqrt(average)        =" << sqrt(avg) <<endl;
+ cout << "Rounded(2 decimals)  =" << round(avg * 100.0) / 100.0 << endl;
 
 
 
